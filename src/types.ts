444f6cb77842833561178093be759751af4e64f3
Trần Ngọc Huy <truongduchuy910@gmail.com>
2024-09-13 10:43:46 +0700

    feat: add graphql type

---
+++ src/types.ts
@@ -0,0 +1,55 @@
+import type { FilterQuery, SortOrder } from 'mongoose';
+
+export interface FindManyProps<T = any> {
+  filter: FilterQuery<T>;
+  paging: PagingInputInterface;
+  sort?: {
+    key: keyof T;
+    KeyType: any;
+    order: SortOrder;
+  };
+  search?: string;
+  access?: any;
+}
+
+export type Sort<T> = { [K in keyof T]: SortOrder | 'ASC' | 'DESC' };
+
+export class CursorInputInterface {
+  after?: string;
+
+  before?: string;
+}
+
+export interface PagingInputInterface {
+  until?: Date;
+
+  since?: Date;
+
+  limit?: number;
+
+  offset?: number;
+
+  cursors?: CursorInputInterface;
+
+  size?: number;
+
+  search?: string;
+
+  sortBy?: string;
+}
+
+export interface PagingProps<T> {
+  filter: any;
+
+  key?: keyof T;
+  keyBuilder?: any;
+  keyOrder?: any;
+
+  order?: SortOrder;
+  search?: string;
+  cursors?: {
+    after?: any;
+    before?: any;
+  };
+  toEntity?: any;
+}
